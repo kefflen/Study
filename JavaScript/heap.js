c3bf5e9b1f54017391e53da1e13b64c13033f920
kefflen <55823300+kefflen@users.noreply.github.com>
2020-12-14 16:06:34 -0300

    update

---
+++ JavaScript/heap.js
@@ -0,0 +1,146 @@
+
+/*
+https://www.youtube.com/watch?v=dM_JHpfFITs
+https://codepen.io/beaucarnes/pen/JNvENQ?editors=0010
+left child: i * 2
+right child: i * 2 + 1
+parent: i / 2
+Quando inserir algo no topo de heap para decer na posição correta
+    precisa verificar qual lado tem o menor
+Ao inserir algum dado, este sera colocado no final do Array heap
+Os "filhos" sempre devem ser menores que seu "pai"
+A lista "heap" começa no 1?
+
+
+Tentar fazer o meu depois
+*/
+
+let MinHeap = function() {
+	
+	let heap = [null];
+	
+	this.insert = function(num) {
+		heap.push(num);
+		if (heap.length > 2) {
+			let idx = heap.length - 1;
+			while (heap[idx] < heap[Math.floor(idx/2)]) {
+				if (idx >= 1) {
+					[heap[Math.floor(idx/2)], heap[idx]] = [heap[idx], heap[Math.floor(idx/2)]];
+					if (Math.floor(idx/2) > 1) {
+						idx = Math.floor(idx/2);
+					} else {
+						break;
+					};
+				};
+			};
+		};
+	};
+	
+	this.remove = function() {
+		let smallest = heap[1];
+		if (heap.length > 2) {
+			heap[1] = heap[heap.length - 1];
+			heap.splice(heap.length - 1);
+			if (heap.length == 3) {
+				if (heap[1] > heap[2]) {
+					[heap[1], heap[2]] = [heap[2], heap[1]];
+				};
+				return smallest;
+			};
+			let i = 1;
+			let left = 2 * i;
+			let right = 2 * i + 1;
+			while (heap[i] >= heap[left] || heap[i] >= heap[right]) {
+				if (heap[left] < heap[right]) {
+					[heap[i], heap[left]] = [heap[left], heap[i]];
+					i = 2 * i
+				} else {
+					[heap[i], heap[right]] = [heap[right], heap[i]];
+					i = 2 * i + 1;
+				};
+				left = 2 * i;
+				right = 2 * i + 1;
+				if (heap[left] == undefined || heap[right] == undefined) {
+					break;
+				};
+			};
+		} else if (heap.length == 2) {
+			heap.splice(1, 1);
+		} else {
+			return null;
+		};
+		return smallest;
+	};
+  
+	this.sort = function() {
+		let result = new Array();
+		while (heap.length > 1) {
+			result.push(this.remove());
+		};
+		return result;
+	};
+
+};
+
+let MaxHeap = function() {
+	
+	let heap = [null];
+	
+	this.print = () => heap;
+
+	this.insert = function(num) {
+		heap.push(num);
+		if (heap.length > 2) {
+			let idx = heap.length - 1;
+			while (heap[idx] > heap[Math.floor(idx/2)]) {
+				if (idx >= 1) {
+					[heap[Math.floor(idx/2)], heap[idx]] = [heap[idx], heap[Math.floor(idx/2)]];
+					if (Math.floor(idx/2) > 1) {
+						idx = Math.floor(idx/2);
+					} else {
+						break;
+					};
+				};
+			};
+		};
+	};
+	
+	this.remove = function() {
+		let smallest = heap[1];
+		if (heap.length > 2) {
+			heap[1] = heap[heap.length - 1];
+			heap.splice(heap.length - 1);
+			if (heap.length == 3) {
+				if (heap[1] < heap[2]) {
+					[heap[1], heap[2]] = [heap[2], heap[1]];
+				};
+				return smallest;
+			};
+			let i = 1;
+			let left = 2 * i;
+			let right = 2 * i + 1;
+			while (heap[i] <= heap[left] || heap[i] <= heap[right]) {
+				if (heap[left] > heap[right]) {
+					[heap[i], heap[left]] = [heap[left], heap[i]];
+					i = 2 * i
+				} else {
+					[heap[i], heap[right]] = [heap[right], heap[i]];
+					i = 2 * i + 1;
+				};
+				left = 2 * i;
+				right = 2 * i + 1;
+				if (heap[left] == undefined || heap[right] == undefined) {
+					break;
+				};
+			};
+		} else if (heap.length == 2) {
+			heap.splice(1, 1);
+		} else {
+			return null;
+		};
+		return smallest;
+	};
+
+};
+
+
